459c0bfc531fd0272e81cdf03bbf907c9bc4ffd4
Javier Woodhouse <javier@wingaru.com.au>
2018-02-19 14:16:57 +1100

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='simplerr',
+      version='0.1',
+      description='The simple request web framework',
+      url='https://github.com/yevrah/simplerr',
+      author='Javier Woodhouse',
+      author_email='javier@wingaru.com.au',
+      license='MIT',
+      packages=['simplerr'],
+      zip_safe=False)
