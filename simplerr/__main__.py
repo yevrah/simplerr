0969bb0a80353b28d06f57b2df24990f0fb04c3a
Javier Woodhouse <yevrah@gmail.com>
2019-11-26 20:39:21 +1100

    nice startup info

--- simplerr/__main__.py
+++ simplerr/__main__.py
@@ -1,42 +1,79 @@
 #!/usr/bin/env python
-"""
-Example usage
-
-./manage.py runserver --site ./examples/contacts
-"""
 import click
-import os
-from simplerr import dispatcher
+import os, sys
+from simplerr import dispatcher, __version__ as sversion
 
 
 @click.group()
-def cli(): pass
+def cli():
+    pass
+
 
 @cli.command()
-@click.option('-s', '--site', type=str, default='/', help='/app_path')
-@click.option('-h', '--hostname', type=str, default='localhost', help="localhost")
-@click.option('-p', '--port', type=int, default=9000, help="9000")
-@click.option('--reloader', is_flag=True, default=True)
-@click.option('--debugger', is_flag=True, default=False)
-@click.option('--evalex', is_flag=True, default=False)
-@click.option('--threaded', is_flag=True)
-@click.option('--processes', type=int, default=1, help="1")
+@click.option("-s", "--site", type=str, default="/", help="/app_path")
+@click.option("-h", "--hostname", type=str, default="localhost", help="localhost")
+@click.option("-p", "--port", type=int, default=9000, help="9000")
+@click.option("--reloader", is_flag=True, default=True)
+@click.option("--debugger", is_flag=True, default=False)
+@click.option("--evalex", is_flag=True, default=False)
+@click.option("--threaded", is_flag=True)
+@click.option("--processes", type=int, default=1, help="1")
 def runserver(site, hostname, port, reloader, debugger, evalex, threaded, processes):
 
-    basedir = os.path.abspath( os.getcwd() ) + site
+    basedir = os.path.abspath(os.getcwd()) + site
 
     """Start a new development server."""
-    app = dispatcher.wsgi(basedir, hostname, port,
-               use_reloader=reloader,
-               use_debugger=debugger,
-               use_evalex=evalex,
-               threaded=threaded,
-               processes=processes) # , ssl_context=(crt, key))
+    app = dispatcher.wsgi(
+        basedir,
+        hostname,
+        port,
+        use_reloader=reloader,
+        use_debugger=debugger,
+        use_evalex=evalex,
+        threaded=threaded,
+        processes=processes,
+    )  # , ssl_context=(crt, key))
 
     app.serve()
 
 
+def before_reload():
+    if os.environ.get("WERKZEUG_RUN_MAIN") != "true":
+        banner()
 
-if __name__ == '__main__':
+
+def after_reload():
+    if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+        # The reloader has restarted, we can put init code here
+        pass
+
+
+def banner():
+    print(
+        """
+               __                __
+        .-----|__.--------.-----|  .-----.----.----.
+        |__ --|  |        |  _  |  |  -__|   _|   _|
+        |_____|__|__|__|__|   __|__|_____|__| |__|
+                          |__| v{}
+
+        Welcome to simplerr! The framework aiming to
+        keep it easy, clean and simple!
+
+        You run `python -m simplerr runserver` with
+        additional parameters, for example:
+
+          `--site project/website` set a web path
+          `--host 192.168.0.1` to specify bind host
+          `--port 8080` to open specific port
+          `--help` to see more options
+    """.format(
+            sversion
+        )
+    )
+
+
+if __name__ == "__main__":
+    before_reload()
+    after_reload()
     cli()
-
--- simplerr/web.py
+++ simplerr/web.py
@@ -3,17 +3,14 @@ import mimetypes
 import functools
 from pathlib import Path
 
-from werkzeug.wrappers import Request, Response
+from werkzeug.wrappers import Response
 from werkzeug.wsgi import wrap_file
 from werkzeug.exceptions import abort
 from werkzeug.routing import Map, Rule
 from werkzeug.utils import redirect as wz_redirect
 
-from .script import script
 from .template import Template
-from .cors import CORS
-from .methods import GET, POST, DELETE, PUT, PATCH, BaseMethod
-from .serialise import json_serial, tojson
+from .serialise import tojson
 from .errors import ToManyArgumentsError
 
 # TODO: Get rid of this dependancy
@@ -22,11 +19,6 @@ from peewee import ModelSelect
 from playhouse.shortcuts import model_to_dict, dict_to_model
 
 
-#  _   _   _   _  _  _   _ _ _  ___  ___   ___  _  _ _  ___  _  _  _  __
-# | \_/ | / \ | || \| | | | | || __|| o ) | o \/ \| | ||_ _|| || \| |/ _|
-# | \_/ || o || || \\ | | V V || _| | o \ |   ( o ) U | | | | || \\ ( |_n
-# |_| |_||_n_||_||_|\_|  \_n_/ |___||___/ |_|\\\_/|___| |_| |_||_|\_|\__/
-#
 class web(object):
     """Primary routing decorator and helpers
 
@@ -121,11 +113,21 @@ class web(object):
     def restore_presets():
         web.destinations = []
 
-    def __init__(self, *args, route=None, template=None, methods=None, endpoint=None, file=False, cors=None, mimetype=None):
+    def __init__(
+        self,
+        *args,
+        route=None,
+        template=None,
+        methods=None,
+        endpoint=None,
+        file=False,
+        cors=None,
+        mimetype=None
+    ):
 
         self.endpoint = endpoint
         self.fn = None
-        self.args = None # to be set when matched() is called
+        self.args = None  # to be set when matched() is called
         self.file = file
         self.cors = cors
         self.mimetype = mimetype
@@ -133,7 +135,7 @@ class web(object):
         # We can specify route, template and methods using **kwargs
         self.route = route
         self.template = template
-        self.methods = methods     # Methods should be left as None to accept all
+        self.methods = methods  # Methods should be left as None to accept all
 
         # However, we also allow a basic grammer with optional arguments, for example:
         #
@@ -155,7 +157,11 @@ class web(object):
 
         # We have to check not string first as issubclass fails on testing str
         # items - This extracts GET/POST which are the only non-string types expected
-        args_methods = [item for item in args if not(isinstance(item, str)) and issubclass(item, BaseMethod)]
+        args_methods = [
+            item
+            for item in args
+            if not (isinstance(item, str)) and issubclass(item, BaseMethod)
+        ]
 
         # Aappend all methods into self.methods
         if len(args_methods) > 0:
@@ -163,20 +169,19 @@ class web(object):
             for method in args_methods:
                 self.methods.append(method.verb)
 
-
         # Only one string, maybe a route or template - default to route if not
         # already populated.
         if len(args_strings) == 1:
-            if(self.route is None):
+            if self.route is None:
                 self.route = args_strings[0]
-            elif(self.template is None):
+            elif self.template is None:
                 self.template = args_strings[0]
             else:
                 raise ToManyArgumentsError("Got too many string arguments")
 
         # Two strings - definately should be a route and a template
         if len(args_strings) == 2:
-            if(self.route is None and self.template is None):
+            if self.route is None and self.template is None:
                 self.route, self.template = args_strings
             else:
                 raise ToManyArgumentsError("Got too many string arguments")
@@ -186,13 +191,12 @@ class web(object):
         if len(args_strings) > 2:
             raise ToManyArgumentsError("Got too many string arguments")
 
-
-
-
     def __call__(self, fn):
         # A quick cleanup first, if no endpoint was specified we need to set it
         # to the view function
-        self.endpoint = self.endpoint or id(fn) # Default endpoint name if none provided.
+        self.endpoint = self.endpoint or id(
+            fn
+        )  # Default endpoint name if none provided.
 
         # Proceed to create decorator
         self.fn = fn
@@ -231,7 +235,6 @@ class web(object):
 
         return match
 
-
     @staticmethod
     def process(request, environ, cwd):
         # tid(f'web.process:(r, e cwd={cwd})')
@@ -262,40 +265,38 @@ class web(object):
 
         # Check to see if this is a peewee model and convert to
         # dict,
-        if(isinstance(data, Model)):
+        if isinstance(data, Model):
             out = model_to_dict(out)
             data = out
 
-
-        if(isinstance(data, ModelSelect)):
+        if isinstance(data, ModelSelect):
             array_out = []
             for item in data:
                 array_out.append(model_to_dict(item))
-                out = {'results': array_out}
+                out = {"results": array_out}
                 data = out
 
-
         # Template expected, attempt render
-        if(template != None):
+        if template != None:
             # Add request to data
             data = data or {}
-            data['request'] = request
+            data["request"] = request
             out = web.template(cwd, template, data)
 
             response = Response(out)
-            response.headers['Content-Type'] = 'text/html;charset=utf-8'
+            response.headers["Content-Type"] = "text/html;charset=utf-8"
 
             # TODO: make reponse plugin based, so cors needs to be added - pre-respon
-            if cors: cors.set(response)
+            if cors:
+                cors.set(response)
 
             return response
 
-
         # Reference example implementation here
         #   http://bit.ly/2ocHYNZ
-        if(file == True):
+        if file == True:
             file_path = Path(cwd) / Path(out)
-            file = open(file_path.absolute().__str__(), 'rb')
+            file = open(file_path.absolute().__str__(), "rb")
             data = wrap_file(environ, file)
 
             mtype = mimetype or mimetypes.guess_type(file_path.__str__())[0]
@@ -303,53 +304,54 @@ class web(object):
             # Sometimes files are named without extensions in the local storage, so
             # instead try and infer from the route
             if mtype is None:
-                urifile = environ.get('PATH_INFO').split('/')[-1:][0]
+                urifile = environ.get("PATH_INFO").split("/")[-1:][0]
                 mtype = mimetypes.guess_type(urifile)[0]
 
-
             response = Response(data, direct_passthrough=True)
-            response.headers['Content-Type'] = '{};charset=utf-8'.format(mtype)
-            response.headers['Cache-Control'] = 'public, max-age=10800'
+            response.headers["Content-Type"] = "{};charset=utf-8".format(mtype)
+            response.headers["Cache-Control"] = "public, max-age=10800"
 
-            if cors: cors.set(response)
+            if cors:
+                cors.set(response)
             return response
 
         # No template, just plain old string response
         if isinstance(data, str):
             response = Response(data)
-            response.headers['Content-Type'] = 'text/html;charset=utf-8'
-            if cors: cors.set(response)
+            response.headers["Content-Type"] = "text/html;charset=utf-8"
+            if cors:
+                cors.set(response)
             return response
-
 
         # Just raw data, send as is
         # TODO: Must be flagged as json explicity
         out = tojson(data)
         response = Response(out)
-        response.headers['Content-Type'] = 'application/json'
-        if cors: cors.set(response)
+        response.headers["Content-Type"] = "application/json"
+        if cors:
+            cors.set(response)
         return response
 
     @staticmethod
     def response(data, *args, **kwargs):
         # TODO: This should build a web() compliant response object
         # that handles cors, additional headers, etc
-        response=Response(data, *args, **kwargs)
+        response = Response(data, *args, **kwargs)
         # if cors: cors.set(response)
-
 
         return response
 
     @staticmethod
     def filter(name):
-
         def wrap(fn):
             # Add to filters dict
             web.filters[name] = fn
 
             def decorated(*args, **kwargs):
                 fn(*args, **kwargs)
+
             return decorated
+
         return wrap
 
         @functools.wraps(fn)
@@ -383,4 +385,3 @@ class web(object):
     @staticmethod
     def all():
         return web.destination
-
